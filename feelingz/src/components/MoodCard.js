39e261b6596ad410883fc26774b7405a88e2c300
michermd <michermd@gmail.com>
2019-01-17 15:25:45 -0500

    Updated key props from mood card to fix bug

--- feelingz/src/components/MoodCard.js
+++ feelingz/src/components/MoodCard.js
@@ -1,26 +1,32 @@
-import React, { Component } from 'react';
-import { Card, Image, Segment } from 'semantic-ui-react'
-import { createMoodCard } from '../actions'
-import { connect } from 'react-redux'
-
+import React, { Component } from "react";
+import { Card, Image, Segment } from "semantic-ui-react";
+import { createMoodCard } from "../actions";
+import { connect } from "react-redux";
 
 class MoodCard extends Component {
-
   render() {
     return (
       <Card>
         <Segment>
-          <Image src={this.props.mood.attributes['img-url']} />
+          <Image src={this.props.mood.attributes["img-url"]} />
         </Segment>
         <Card.Content>
           <Card.Header>{this.props.mood.attributes.emotion}</Card.Header>
           <Card.Meta>
-            <span className='date'>Created: {this.props.mood.attributes['created-at']}</span>
+            <span className="date">
+              Created: {this.props.mood.attributes["created-at"]}
+            </span>
           </Card.Meta>
           <Segment>
-              <Card.Description>My Mood: {this.props.mood.attributes.mood}</Card.Description>
-              <Card.Description>Activity Level: {this.props.mood.attributes.activity}</Card.Description>
-              <Card.Description>Journal: {this.props.mood.attributes.journal}</Card.Description>
+            <Card.Description>
+              My Mood: {this.props.mood.attributes.mood}
+            </Card.Description>
+            <Card.Description>
+              Activity Level: {this.props.mood.attributes.activity}
+            </Card.Description>
+            <Card.Description>
+              Journal: {this.props.mood.attributes.journal}
+            </Card.Description>
           </Segment>
         </Card.Content>
       </Card>
@@ -28,5 +34,7 @@ class MoodCard extends Component {
   }
 }
 
-
-export default connect(null, { createMoodCard }) (MoodCard);
+export default connect(
+  null,
+  { createMoodCard }
+)(MoodCard);
--- feelingz/src/components/MyMoods.js
+++ feelingz/src/components/MyMoods.js
@@ -1,58 +1,64 @@
-import React, { Component } from 'react';
-import MoodCard from './MoodCard'
-import MoodChart from './MoodChart'
-import { connect } from 'react-redux'
-import { getUserMoods } from '../actions'
-import { Grid, Container, Segment, Button } from 'semantic-ui-react';
+import React, { Component } from "react";
+import MoodCard from "./MoodCard";
+import MoodChart from "./MoodChart";
+import { connect } from "react-redux";
+import { getUserMoods } from "../actions";
+import { Grid, Container, Segment, Button } from "semantic-ui-react";
 
 class MyMoods extends Component {
-
   componentDidMount = () => {
     fetch(`http://localhost:3001/api/v1/mood_posts/`)
-    .then(res => res.json())
-    .then(userMoods => {
-      this.props.getUserMoods(userMoods.data)
-    })
-  }
-
-
+      .then(res => res.json())
+      .then(userMoods => {
+        this.props.getUserMoods(userMoods.data);
+      });
+  };
 
   render() {
     // console.log(this.props);
     return (
       <div>
         <Container>
-          <Segment >
+          <Segment>
             <center>
-              <Button className='btn' basic color="purple" onClick={() => this.props.history.push('/index')}>Create a New Mood</Button>
+              <Button
+                className="btn"
+                basic
+                color="purple"
+                onClick={() => this.props.history.push("/index")}
+              >
+                Create a New Mood
+              </Button>
             </center>
           </Segment>
           <Segment>
             <Grid relaxed columns={4}>
-              { this.props.user_moods.map(mood => {
-                // console.log('mood id', mood.id);
+              {this.props.user_moods.map(mood => {
+                // console.log("mood id", mood.id);
                 return (
-                  <Grid.Column>
-                    <MoodCard key={mood.id} mood={mood}/>
-                  </Grid.Column>)
-                })
-              }
+                  <Grid.Column key={mood.id}>
+                    <MoodCard mood={mood} />
+                  </Grid.Column>
+                );
+              })}
             </Grid>
           </Segment>
           <Segment>
-            <MoodChart/>
+            <MoodChart />
           </Segment>
         </Container>
       </div>
     );
   }
-
 }
 
-const mapsStateToProps = (state) => {
+const mapsStateToProps = state => {
   return {
     user_moods: state.user_moods
-  }
-}
+  };
+};
 
-export default connect(mapsStateToProps, { getUserMoods }) (MyMoods);
+export default connect(
+  mapsStateToProps,
+  { getUserMoods }
+)(MyMoods);
