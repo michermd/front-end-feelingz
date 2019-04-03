032c242ae0e1163b375dd0010b9fec74094407f0
michermd <michermd@gmail.com>
2019-04-03 13:21:04 -0400

    Revert "Update index.js"
    This reverts commit aac4e619d116591124ebad8491bcd698bf353bc0.

--- feelingz/src/index.js
+++ feelingz/src/index.js
@@ -1,29 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import { BrowserRouter as Router } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router } from 'react-router-dom';
 // import { composeWithDevTools } from 'redux-devtools-extension';
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-import reducer from "./components/reducer";
-import "./index.css";
-import "semantic-ui-css/semantic.min.css";
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './components/reducer'
+import './index.css';
+import 'semantic-ui-css/semantic.min.css';
 
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+
+
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 // console.log('store', store)
 // console.log('state', store.getState())
+
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
       <App />
     </Router>
-  </Provider>,
-  document.getElementById("root")
-);
+  </Provider>, document.getElementById('root'));
 registerServiceWorker();
